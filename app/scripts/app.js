57a207c4707cdcad2bc58ce8098b526bafa6aac1
letuyet <letuyet1928@gmail.com>
2014-04-18 17:22:41 +0700

    Modified   app/scripts/app.js Modified   app/scripts/controllers/question-list-controller.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,10 @@ function QAPRouteConfig($routeProvider) {
 		controller: 'QuestionsListController',
 		templateUrl: 'views/questions-list.html'
 	})
+	.when('/category/:id', {
+		controller: 'QuestionsListController',
+		templateUrl: 'views/questions-list.html'
+	})
 	.when('/ask-question', {
 		controller: 'AskQuestionController',
 		templateUrl: 'views/ask-question.html'
