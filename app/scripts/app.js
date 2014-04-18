c0fd90417ea3cb06e387bf8bed69697214d75879
letuyet <letuyet1928@gmail.com>
2014-04-18 17:10:58 +0700

    Modified   app/index.html Modified   app/scripts/app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,35 @@
-var QAP = angular.module('QAP',['QuestionsList']);
+var QAP = angular.module('QAP', ['QuestionsList']);
 
 function QAPRouteConfig($routeProvider) {
 
-	$routeProvider
-	.when('/', {
-		controller: 'QuestionsListController',
-		templateUrl: 'views/questions-list.html'
-	})
-	.otherwise({
-		redirectTo: '/'
-	});
+    $routeProvider
+        .when('/', {
+            controller: 'QuestionsListController',
+            templateUrl: 'views/questions-list.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }
 
 QAP.config(QAPRouteConfig);
+
+QAP.controller('CategoriesListController', function($scope) {
+        // Category
+        $scope.categoryList = [
+        	{id: '01',name: 'Hardware', number:'1'}, 
+        	{id: '02',name: 'Software', number:'5'},
+        	{id: '03',name: 'Life experiences', number:'8'},
+        	{id: '04',name: 'The love', number:'10'}, 
+        	{id: '05',name: 'The family', number:'15'},
+        	{id: '06',name: 'Beauty', number:'20'},
+        	{id: '07',name: 'Entertainment', number:'11'},
+        	{id: '08',name: 'Motherhood', number:'12'}
+       	];
+       	$scope.selectedCategory = '01';
+
+       	$scope.selectCategory = function(categoryId){
+			$scope.selectedCategory = categoryId;		
+       	}
+
+    });
