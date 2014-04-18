cc2a1aff7ebdc241ff615de849e3119d303a755f
HoaNguyen <thaihoa311@gmail.com>
2014-04-18 16:55:10 +0700

    Modified   app/scripts/controllers/question-list-controller.js Modified   app/scripts/services.js

--- app/scripts/controllers/question-list-controller.js
+++ app/scripts/controllers/question-list-controller.js
@@ -1,60 +1,9 @@
-var QuestionsList = angular.module('QuestionsList',[]);
+var QuestionsList = angular.module('QuestionsList',['services']);
 
-QuestionsList.controller('QuestionsListController', function($scope) {
-	$scope.questions = [
-	    {'id': '01',
-		'title': 'Question 1',
-		'user': "user 1",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '02',
-		'title': 'Question 2',
-		'user': "user 2",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '03',
-		'title': 'Question 3',
-		'user': "user 3",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '04',
-		'title': 'Question 4',
-		'user': "user 4",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '05',
-		'title': 'Question 5',
-		'user': "user 5",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '06',
-		'title': 'Question 6',
-		'user': "user 6",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '07',
-		'title': 'Question 7',
-		'user': "user 7",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '08',
-		'title': 'Question 8',
-		'user': "user 8",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '09',
-		'title': 'Question 9',
-		'user': "user 9",
-		'date': '20-04-2014',
-		'anwsers': 12},
-		{'id': '10',
-		'title': 'Question 10',
-		'user': "user 10",
-		'date': '20-04-2014',
-		'anwsers': 12}
-	];
+QuestionsList.controller('QuestionsListController', function($scope, Questions) {
+	$scope.questions = Questions.query();
 });
 
-QuestionsList.controller('AskQuestionController', function($scope) {
+QuestionsList.controller('AskQuestionController', function($scope, Questions) {
 	console.log('AskQuestionController is running');
 });
