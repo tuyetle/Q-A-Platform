bca99a07839b09b1dcf619ece1d75ca6326163ed
HoaNguyen <thaihoa311@gmail.com>
2014-04-18 16:37:51 +0700

    Modified   app/index.html Modified   app/scripts/app.js Modified   app/scripts/controllers/question-list-controller.js Modified   app/scripts/services.js Modified   app/views/questions-list.html

--- app/scripts/controllers/question-list-controller.js
+++ app/scripts/controllers/question-list-controller.js
@@ -3,3 +3,7 @@ var QuestionsList = angular.module('QuestionsList',[]);
 QuestionsList.controller('QuestionsListController', function($scope) {
 	
 });
+
+QuestionsList.controller('AskQuestionController', function($scope) {
+	console.log('AskQuestionController is running');
+});
