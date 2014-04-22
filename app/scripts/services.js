90fa1729bba7bebca0500588f0e0b32fe3e44402
dluyllong <lduy_llong@yahoo.com.vn>
2014-04-22 14:59:23 +0700

    question detail

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,17 +1,61 @@
 var ServicesModule = angular.module('services', []);
 
+ServicesModule.factory('Users',function () {
+	var data = [
+		{
+			id: 1,
+			name: 'Hoa',
+			joinDate: '01-01-2014',
+			point: 100
+		},
+		{
+			id: 2,
+			name: 'Tuyet',
+			joinDate: '01-01-2014',
+			point: 100
+		},
+		{
+			id: 3,
+			name: 'Tuyen',
+			joinDate: '01-01-2014',
+			point: 100
+		},
+		{
+			id: 4,
+			name: 'Thao',
+			joinDate: '01-01-2014',
+			point: 100
+		},
+		{
+			id: 5,
+			name: 'Long',
+			joinDate: '01-01-2014',
+			point: 100
+		}
+	];
+	
+	var Users = {
+		query: function() {
+			return data;
+		},
+		getCurrentUser: function () {
+			return 5;
+		}
+	}
+	return Users;
+});
 
 ServicesModule.factory('Questions',function() {
 	var data = [{
-		id: '01',
-		title: 'Oldest daughter cast in local TV sitcom The Gellers Wants to be actress?',
-		description: "",
-		user: "Adam Johnson",
+		id: 1,
+		title: 'Ai gấu nhất ở đây?',
+		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		user: "Long",
 		date: '20-04-2014',
-		anwsers: 2,
+		anwsers: 5,
 		categoryIDs: ['01']
 	}, {
-		id: '02',
+		id: 2,
 		title: 'Why international public opinion did not support the Hungarian Revolution of 1956?',
 		description: 'Why why why why ?',
 		user: "Tu4n",
@@ -19,7 +63,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 3,
 		categoryIDs: ['01','10']
 	}, {
-		id: '03',
+		id: 3,
 		title: 'How long do you think it would take the average person to be able to learn to draw from a living?',
 		description: 'I want to draw good enough that I can make a living from it and Im starting my drawing training today although I have been drawing(hundreds of drawings) since last year but not as serious as Im about to get now as I need to be good enough to make a living at it. Im curious as to what you think about this. Im a young adult by the way.',
 		user: "user 3",
@@ -27,7 +71,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 1,
 		categoryIDs: ['01']
 	}, {
-		id: '04',
+		id: 4,
 		title: 'When will be Germany and Austria available in Google Street View?',
 		description: 'Almost all others country are already available, but Germany and Austria still not. I dont understand why...:-(',
 		user: "Jaloslav",
@@ -35,7 +79,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 4,
 		categoryIDs: ['05']
 	}, {
-		id: '05',
+		id: 5,
 		title: 'Download flappy bird?',
 		description: 'Is it still available anywhere? or is it gone forever because I accidentally deleted...',
 		user: "Joe",
@@ -43,7 +87,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 2,
 		categoryIDs: ['05']
 	}, {
-		id: '06',
+		id: 6,
 		title: 'How to fix dissapeared text and folders in window 7?',
 		description: 'I accedently sitched off my pc directly through ups! after that my pc recoverd automatically! after that i cant see any text under folders and icons in desktop! my start menu dosent open,...',
 		user: "cammy",
@@ -51,7 +95,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 7,
 		categoryIDs: ['05, 10']
 	}, {
-		id: '07',
+		id: 7,
 		title: 'What are your top ten Disney movies?',
 		description: 'Hi I was thinking \'bout having a Disney movie marathon and I was wondering if you could give me your top ten...',
 		user: "Rick",
@@ -59,7 +103,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 2,
 		categoryIDs: ['09']
 	}, {
-		id: '08',
+		id: 8,
 		title: 'Why do we lose our interest for a song?',
 		description: 'So i might find a song i love to pieces, and upon hearing it i feel like my life is complete. But after listening to it over and over it gets old and don\'t enjoy it...',
 		user: "Nicky",
@@ -67,7 +111,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 0,
 		categoryIDs: ['09']
 	}, {
-		id: '09',
+		id: 9,
 		title: 'Do you know what song this is?',
 		description: 'youtube.com/watch?v=HoYA68W0dcY Bum ba la bum - can\'t get it out of my head and need more :3',
 		user: "Shao",
@@ -75,7 +119,7 @@ ServicesModule.factory('Questions',function() {
 		anwsers: 0,
 		categoryIDs: ['01']
 	}, {
-		id: '10',
+		id: 10,
 		title: 'The Walking Dead Comics?',
 		description: 'I have read all of The Walking Dead comics online so far. But I want to start getting a copy and reading them.',
 		user: "Psycho",
@@ -98,6 +142,13 @@ ServicesModule.factory('Questions',function() {
 			});
 
 			return arrayResult;
+		},
+		getQuestionDetail: function (id) {
+			for (var i = 0; i < data.length; i++) {
+   				if ( data[i].id == id ) {
+					return data[i];
+				}
+   			};
 		}
 	}
 	return Questions;
@@ -129,4 +180,129 @@ ServicesModule.factory('Categories', function() {
    		}
    	};
 	return Categories;
+});
+
+ServicesModule.factory('Answers', function() {
+	var data = {
+		1:[
+			{
+				'id': 1,
+				'date': '22-04-2014',
+				'author': 'Tuyet',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 2,
+				'date': '22-04-2014',
+				'author': 'Thao',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 3,
+				'date': '22-04-2014',
+				'author': 'Tuyen',
+				'content': 'Tui không biết!',
+				'point': 8
+			},
+			{
+				'id': 4,
+				'date': '22-04-2014',
+				'author': 'Hoa',
+				'content': 'Tui không biết!',
+				'point': 5
+			},
+			{
+				'id': 5,
+				'date': '22-04-2014',
+				'author': 'Long',
+				'content': 'Tui không biết!',
+				'point': -6
+			}
+		],
+		2:[
+			{
+				'id': 6,
+				'date': '22-04-2014',
+				'author': 'Tuyet',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 7,
+				'date': '22-04-2014',
+				'author': 'Thao',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 8,
+				'date': '22-04-2014',
+				'author': 'Tuyen',
+				'content': 'Tui không biết!',
+				'point': 8
+			},
+			{
+				'id': 9,
+				'date': '22-04-2014',
+				'author': 'Hoa',
+				'content': 'Tui không biết!',
+				'point': 5
+			},
+			{
+				'id': 10,
+				'date': '22-04-2014',
+				'author': 'Long',
+				'content': 'Tui không biết!',
+				'point': -6
+			}
+		],
+		3:[
+			{
+				'id': 11,
+				'date': '22-04-2014',
+				'author': 'Tuyet',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 12,
+				'date': '22-04-2014',
+				'author': 'Thao',
+				'content': 'Tui không biết!',
+				'point': 10
+			},
+			{
+				'id': 13,
+				'date': '22-04-2014',
+				'author': 'Tuyen',
+				'content': 'Tui không biết!',
+				'point': 8
+			},
+			{
+				'id': 14,
+				'date': '22-04-2014',
+				'author': 'Hoa',
+				'content': 'Tui không biết!',
+				'point': 5
+			},
+			{
+				'id': 15,
+				'date': '22-04-2014',
+				'author': 'Long',
+				'content': 'Tui không biết!',
+				'point': -6
+			}
+		]
+	};
+	var Answers = {
+		query: function() {
+			return data;
+		},
+		getAnswersByQuestionId: function(id) {
+			return (data[id].length > 0 ? data[id] : null);
+		}
+	};
+	return Answers;
 });
