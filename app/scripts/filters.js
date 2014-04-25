77606534caacfbb071def7283a818f99ea7a0c36
HoaNguyen <thaihoa311@gmail.com>
2014-04-25 10:24:07 +0700

    Modified   app/scripts/filters.js Modified   app/views/questions-list.html

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,19 +1,17 @@
 var filters = angular.module('filters', []);
 
-filters.filter('trunCate', function() {
+filters.filter('truncate', function() {
 	return function(input, limit) {
-		
 		var finalText = "";
-		var text2 = input.replace(/\s+/g, ' ');
-		var text3 = text2.split(' ');
-		var numberOfWords = text3.length;
+		var words = input.replace(/\s+/g, ' ').split(' ');
+		var numberOfWords = words.length;
 		var i=0;
 		if(numberOfWords > limit)
 		{
-		for(i=0; i< limit; i++)
-		finalText = finalText+" "+ text3[i]+" ";
-
-		return finalText+"...";
+			for (i=0; i < limit; i++) {
+				finalText = finalText+" "+ words[i];
+			}
+			return finalText+"...";
 		}
 		else return input;
 	}
