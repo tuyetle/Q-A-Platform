5c39b6af969e1eef0cff33216e95c6035a8d9302
HoaNguyen <thaihoa311@gmail.com>
2014-04-29 11:02:30 +0700

    Add        app/fonts/glyphicons-halflings-regular.eot Add        app/fonts/glyphicons-halflings-regular.svg Add        app/fonts/glyphicons-halflings-regular.ttf Add        app/fonts/glyphicons-halflings-regular.woff Add        app/images/13858-940.jpg Add        app/images/logo.jpg Add        app/images/sprites.png Add        app/index.html Add        app/scripts/app.js Add        app/scripts/controllers/answer-controller.js Add        app/scripts/controllers/question-controller.js Add        app/scripts/cookies.js Add        app/scripts/directives.js Add        app/scripts/filters.js Add        app/scripts/services.js Add        app/scripts/vendors/angular-cookies.js Add        app/scripts/vendors/angular-resource.js Add        app/scripts/vendors/angular-route.js Add        app/scripts/vendors/angular.js Add        app/scripts/vendors/bootstrap.js Add        app/scripts/vendors/bootstrap.min.js Add        app/scripts/vendors/underscore-min.js Add        app/styles/bootstrap-theme.css Add        app/styles/bootstrap-theme.css.map Add        app/styles/bootstrap-theme.min.css Add        app/styles/bootstrap.css Add        app/styles/bootstrap.css.map Add        app/styles/bootstrap.min.css Add        app/styles/custom.css Add        app/views/ask-question.html Add        app/views/question-details.html Add        app/views/questions-list.html

---
+++ app/scripts/app.js
@@ -0,0 +1,97 @@
+var QAP = angular.module('QAP', ['ngRoute', 'QAP.QuestionControllers', 'QAP.AnswersControllers','ngCookies']);
+
+function QAPRouteConfig($routeProvider) {
+	$routeProvider
+	.when('/', {
+		controller: 'QuestionsListController',
+		templateUrl: 'views/questions-list.html'
+	})
+	.when('/category/:id', {
+		controller: 'QuestionsListController',
+		templateUrl: 'views/questions-list.html'
+	})
+	.when('/q-details/:id', {
+		controller: 'QuestionDetailsController',
+		templateUrl: 'views/question-details.html'
+	})
+	.when('/ask-question', {
+		controller: 'AskQuestionController',
+		templateUrl: 'views/ask-question.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}
+
+QAP.config(QAPRouteConfig);
+QAP.currentUserId = 5;
+QAP.cookies = {
+	'answers': 'answers',
+	'users': 'users',
+	'questions': 'questions',
+	'categories': 'categories'
+};
+
+QAP.controller('MainController', function ($scope, $location, $cookieStore, Questions, Answers, Users, Categories) {
+	$scope.askQuestionPath = '#/ask-question';
+	$scope.$location = $location;
+	$scope.locationPath = $location.path();
+	
+	// CLEAR ALL COOKIES
+	$scope.clearCookies = function () {
+		for(var cookie in QAP.cookies) {
+			$cookieStore.remove(cookie);
+		}
+	};
+	
+	// LOAD COOKIES
+	Questions.query();
+	Answers.query();
+	Users.query();
+	Categories.query();
+});
+
+QAP.controller('CategoriesListController', function ($scope, Categories, $rootScope) {
+    // Category
+    $scope.categoryList = Categories.query();
+
+
+   	$rootScope.selectedCategory = '-1';
+
+   	$scope.selectCategory = function(id) {
+   		$rootScope.selectedCategory = id;
+   	}
+});
+
+QAP.controller('UserController', function ($scope, Users, $rootScope) {
+	$scope.currentUser = null;
+
+	$scope.logined = function() {
+		$scope.currentUser = Users.getCurrentUser()
+		return $scope.currentUser == undefined ? false : true;
+	} 
+	$scope.logout = function() {
+		Users.logout();
+	}
+});
+
+QAP.controller('LoginController', function($scope, Users, $rootScope) {
+
+	$scope.loginSuccessfull = false;
+	$scope.loginClicked = false;
+	$scope.loginUser = null;
+
+	$scope.login = function() {
+		if ($scope.loginForm.$valid) {
+			$scope.loginClicked = true;
+			var user = Users.login($scope.loginUser);
+			if (user) {			
+				$scope.loginSuccessfull = true;
+				angular.element('#modal-login').modal('hide');
+			} else {
+				$scope.loginSuccessfull = false;
+			}
+		}
+	}
+
+});
---
+++ app/scripts/controllers/answer-controller.js
@@ -0,0 +1,60 @@
+var AnswersControllers = angular.module('QAP.AnswersControllers', ['QAP.services','QAP.filters','ngCookies']);
+
+// ANSWER OF QUESTION DETAIL
+AnswersControllers.controller('AnswerItemController', function ($rootScope, $scope, $filter, Answers, Users, $routeParams) {
+
+	$scope.questionId = $routeParams.id;
+	$scope.answer = Answers.getAnswerById($scope.questionId, $scope.answer.id);
+	$scope.answerUser = Users.getUserById($scope.answer.userID);
+	
+	$scope.rateAvai = function() {
+		var currentUser = $rootScope.rootCurrentUser;
+
+		if (currentUser)
+			return $scope.answer.userID != currentUser.id && $scope.answer.ratedBy.indexOf(currentUser.id) < 0;
+		return false;
+	} 
+
+	// LIKE OR DISLIKE ANSWER
+	$scope.rate = function (point) {
+
+		if ( $scope.rateAvai() ) {
+			var currentUserId = $rootScope.rootCurrentUser.id;
+			Answers.rateAnswer($scope.questionId,$scope.answer.id,point,currentUserId);
+			$scope.answerUser.point+=point;
+			Users.save();
+		}
+		return false;
+	};
+});
+
+// ADD YOUR ANSWER
+AnswersControllers.controller('AddAnswerController', function($rootScope, $scope, $filter, Answers, Users, $routeParams) {
+
+
+	$scope.questionId = $routeParams.id;
+	$scope.newAnswerContent = null;
+	
+	$scope.addAnswer = function () {
+		$scope.newAnswer = {
+			'id': null,
+			'date': null,
+			'userID': $rootScope.rootCurrentUser.id,
+			'content': null,
+			'point': 0,
+			'ratedBy': []
+		};
+		var currentDate = $filter('date')(new Date(), 'yyyy-MM-ddThh:mm:ssZ');
+		$scope.newAnswer.date = currentDate;
+		$scope.newAnswer.content = $scope.newAnswerContent;
+		$scope.newAnswerContent = '';
+		Answers.insertAnswer($scope.questionId, $scope.newAnswer);
+		return false;
+	};
+	
+	$scope.resetAnswer = function () {
+		$scope.newAnswerContent = '';
+		return false;
+	};
+
+});
